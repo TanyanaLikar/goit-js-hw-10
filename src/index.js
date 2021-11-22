a2e049ba3234eb18cf89ffabfc009d5933897d61
TatyanaLikar <85054268+TanyanaLikar@users.noreply.github.com>
2021-11-22 21:03:14 +0200

    save

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const DEBOUNCE_DELAY = 300;
 
 
 const inputEl = document.querySelector('#search-box');
-const countryListEL = document.querySelector('.country-list');
-const countryInfoEL = document.querySelector('.country-info');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(onSearchCountry,DEBOUNCE_DELAY));
 
@@ -18,24 +18,44 @@ inputEl.addEventListener('input', debounce(onSearchCountry,DEBOUNCE_DELAY));
 function onSearchCountry(event) {
     event.preventDefault();
     const countryInput = event.target.value.trim();   
-
-    fetchCountries(countryInput)
-    .then(renderCountriesCard);
-    if(countryInput === ''){
-        inputEl.reset();
-    }
-    
-         
+  
+    fetchCountries(countryInput)    
+    .then(showCountry)
+    .catch(onFetchError);
+             
 }
 
-
-function renderCountriesCard(country) {
+function renderCountryCard(country) {
     const markup = countryInfo(country);   
-    countryInfoEL.innerHTML = markup;
+    countryInfoEl.innerHTML = markup;       
 }
+
 function renderCountriesList(countries) {
     countries.forEach(country => {
       const markup = countryList(country);
-      refs.countryList.insertAdjacentHTML('beforeend', markup);
+      countryListEl.insertAdjacentHTML('beforeend', markup);      
     });
+  }
+  function showCountry(countries) {
+    if(countries.length <= 1){  
+        clear()     
+        renderCountryCard(countries);
+        console.log(renderCountryCard())
+    } 
+    if(countries.length > 1 && countries.length <= 10){
+        clear()
+        renderCountriesList(countries)
+    } 
+    if(countries.length > 10) {
+        clear()
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    }  
+  }
+  function onFetchError(error) {
+    clear();
+    Notiflix.Notify.failure('Oops, there is no country with that name!!!');
+  }
+  function clear() {
+    countryInfoEl.innerHTML = '';
+    countryListEl.innerHTML = '';
   }
